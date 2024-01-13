d5661532ea9fca7a5c7027219dbc9767fe577e92
Joonas Viskari <joonas.viskari@gmail.com>
2024-01-14 00:18:52 +0200

    first version

--- documents/release/crossdev/build.sh
+++ documents/release/crossdev/build.sh
@@ -5,9 +5,9 @@ BINDIR=../../../bin
 #
 #		Convert source.bsc to a tokenised program
 #
-python3 $BINDIR/makebasic.zip source.bsc -oprogram.bas
+python3 $BINDIR/makebasic.zip $1.bsc -o$1.bas
 #
 #		Run it in the emulator. Loads program.bas in to 'page' (where program code goes)
 #		Then warm starts the interpreter. You can use exec to auto run it.
 #
-$BINDIR/neo	program.bas@page warm
+$BINDIR/neo	$1.bas@page warm
